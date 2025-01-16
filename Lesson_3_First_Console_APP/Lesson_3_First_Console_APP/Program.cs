a70f37c9047296be15dd4b3cee58a12e2b829afe
Valeh Suleymanov <sadiqkor@hotmail.com>
2025-01-16 16:54:43 +0400

    Third lesson about RAMS, BreakPoint, Region and starting learning variables

---
+++ Lesson_3_First_Console_APP/Lesson_3_First_Console_APP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_First_Console_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valeh qaqa necesen ");
+            Console.Read();
+
+            #region Name
+            #endregion
+
+        }
+    }
+}
---
+++ Lesson_3_First_Console_APP/TaskInlesson1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskInlesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var red = "Qımızı";
+            var car = "Avtomobil";
+            var expencive = "bahali";
+            var phone = "telefon";
+
+            Console.WriteLine(red+" "+car);
+            Console.WriteLine(expencive+" "+phone);
+            Console.ReadLine();
+        }
+    }
+}
